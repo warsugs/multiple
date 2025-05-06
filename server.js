6f6a90912327403d37352412c81945d03ac1f5fe
wawa <putelli88@hotmail.fr>
2025-05-07 00:16:26 +0200

    Correction des URLs pour Render (score et classement)

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Pour lire les données envoyées en JSON
 app.use(express.json());
